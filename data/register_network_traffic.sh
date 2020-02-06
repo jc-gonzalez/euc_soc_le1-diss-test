640b056fdebdc783cfc6b6500e03fbaa52689d9d
J C Gonzalez <JCGonzalez@sciops.esa.int>
2020-02-06 15:56:58 +0100

    New minimal script to register network traffic info

---
+++ data/register_network_traffic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curr_date=$(date +"%Y%m%d_%H%M%S")
+datafile="network_traffic_${curr_date}.dat"
+
+while true ; do
+
+    ifconfig eth0 | \
+        awk -v thedate=$(date +"%Y%m%d_%H%M%S") 'BEGIN{printf "%s ",thedate;}{printf "%s ",$0;}' | \
+        sed -e 's/[ ]+/ /g' >> ${datafile}
+
+    sleep 10
+
+    if [ -f "./STOP" ]; then
+        break
+    fi
+
+done
